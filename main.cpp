a5a2b33e7fc4413c64816db7c952e78449435ede
FilipBor <filibor847@student.polsl.pl>
2020-10-22 15:06:26 +0200

    Adding two matrices and printing result

--- main.cpp
+++ main.cpp
@@ -1,8 +1,54 @@
 #include <iostream>
 #include <vector>
+
+
 struct Matrix2D
 {
    std::vector <std::vector<int>> contents;
+    int cols(){
+        return contents[0].size();
+    }
+    int rows() {
+        return contents.size();
+    }
+   void add(Matrix2D other){
+       if (other.rows() == rows() && other.cols() == cols())
+       {
+           for (int i = 0; i < rows(); i++)
+           {
+               for (int j = 0; j < cols(); j++)
+               {
+                   contents[i][j] += other.contents[i][j];
+               }
+           }
+       }
+       else
+       {
+           std::cout <<"Dimensions must match!"<< std::endl;
+       }
+   }
+
+
+
+   void input_matrix(){
+       int rows, cols;
+       std::cout<<"Introduce number of rows: "<<std::endl;
+       std::cin >> rows;
+       std::cout<<"Introduce number of columns: "<<std::endl;
+       std::cin >> cols;
+       for(int i = 0; i < rows; i++) {
+           std::vector<int> row;
+           int tmp;
+           std::cout <<"Adding a row with "<< cols << " columns"<<std::endl;
+           for (int j = 0; j < cols; j++){
+           std::cout <<"Introduce value: "<<std::endl;
+           std::cin >> tmp;
+           row.push_back(tmp);
+           }
+        contents.push_back(row);
+       }
+   }
+
    void print()
    {
        int rows = contents.size();
@@ -20,15 +66,22 @@ struct Matrix2D
 };
 
 int main(){
-    Matrix2D a;
-    Matrix2D b;
+    Matrix2D a, b;
 
-    a.contents = {{1,5,3}, {4,8,7}, {3,9,4}};
-    b.contents = {{5,2,9}, {3,2,8}, {1,0,3}};
+    std::cout << "Introduce data for matrix a: "<<std::endl;
+    a.input_matrix();
+    std::cout << "Introduce data for matrix b: "<<std::endl;
+    b.input_matrix();
 
+    std::cout << "Printing matrix a: "<<std::endl;
     a.print();
+    std::cout << "Printing matrix b: "<<std::endl;
     b.print();
 
+    a.add(b);
+    std::cout << "Printing added matrices: "<<std::endl;
+
+    a.print();
 
     return 0;
 
