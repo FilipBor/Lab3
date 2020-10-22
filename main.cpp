7359339c97af53ab17c90a273205f05065c71102
FilipBor <filibor847@student.polsl.pl>
2020-10-22 12:23:07 +0200

    Structures creation and accessing elements

--- main.cpp
+++ main.cpp
@@ -1,4 +1,26 @@
+#include <iostream>
 
-int main() {
-    return 0;
+
+struct Point2D
+{
+    double x;
+    double y;
+};
+
+int main (){
+
+    Point2D mypoint;
+    mypoint.x = 645.34;
+    mypoint.y = 43.53;
+
+    std::cout<< "Value of X: " << mypoint.x <<std::endl;
+    std::cout<< "Value of Y: " << mypoint.y <<std::endl;
+
+    Point2D myotherpoint;
+    myotherpoint.x = 5453.54;
+    myotherpoint.y = 4545.34;
+    std::cout<< "Value of X: " << myotherpoint.x <<std::endl;
+    std::cout<< "Value of Y: " << myotherpoint.y <<std::endl;
+
+
 }
